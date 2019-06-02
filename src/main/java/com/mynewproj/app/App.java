66f29faff0af55f48fbe82d7a5c28a9d8d9dde39
admin <bincybabu233@gmail.com>
2019-06-02 05:35:54 +0000

    New

---
+++ src/main/java/com/mynewproj/app/App.java
@@ -0,0 +1,13 @@
+package com.mynewproj.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Bincy ba" );
+    }
+}
